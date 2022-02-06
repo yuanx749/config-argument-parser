70f34284c492e121bb489f6de74115588ebbf3e3
yuanx749 <yuanx749@gmail.com>
2022-02-06 19:10:54 +0100

    Change shield

--- configargparser/__init__.py
+++ configargparser/__init__.py
@@ -1,6 +1,6 @@
 """A package help automatically create command-line interface from configuration or code."""
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 from .cap import ConfigArgumentParser
 
